2881d81e935174046f58759c67eac7e023589076
Tsimafei Petrykevich <petrik3003@gmail.com>
2024-09-14 20:11:07 +0300

    feat: draw glyphs with cubic bezier curves only and extract drawing to renderer struct

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod text;
 
 use text::{
-    render::draw_glyph,
+    render::Renderer,
     ttf::{parse_cmap, parse_font, parse_glyph, parse_glyph_offsets},
     FontFile,
 };
+
+const EM_SIZE: u32 = 2048;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut font_file = FontFile::load("res/Roboto-Regular.ttf")?;
@@ -15,7 +17,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("{:?}", cmap);
 
-    let rendered_char_idx: usize = 66;
+    let rendered_char_idx: usize = 65;
     let char_mapping = cmap[cmap
         .binary_search_by(|mapping| mapping.unicode.cmp(&rendered_char_idx))
         .map_err(|_| {
@@ -34,7 +36,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let glyph = parse_glyph(&mut font_file)?;
     println!("{:?}", glyph);
-    draw_glyph(&glyph)?;
+
+    let mut renderer = Renderer::new(64f32, 64f32, EM_SIZE, EM_SIZE);
+    renderer.draw_glyph(&glyph)?;
 
     Ok(())
 }
--- src/text/render.rs
+++ src/text/render.rs
@@ -1,74 +1,82 @@
 use std::{fs::File, io::Write};
 
-use tiny_skia::{Paint, PathBuilder, Pixmap, Stroke, Transform};
+use tiny_skia::{Paint, PathBuilder, Pixmap, Point, Stroke, Transform};
 
-use super::font_data::GlyphData;
+use super::{font_data::GlyphData, GlyphPoint};
 
-const EM_SIZE: u32 = 2048;
+pub struct Renderer {
+    pub offset: Point,
+    pub pixmap: Pixmap,
+    pub stroke: Stroke,
+}
 
-pub fn draw_glyph(glyph_data: &GlyphData) -> Result<(), Box<dyn std::error::Error>> {
-    let width = EM_SIZE;
-    let height = EM_SIZE;
-    let offset_x = EM_SIZE as f32 / 32f32;
-    let offset_y = EM_SIZE as f32 / 32f32;
-
-    let mut pixmap = Pixmap::new(width, height).unwrap();
-    let mut contours = Vec::with_capacity(glyph_data.contour_end_indices.len());
-
-    let paint = Paint {
-        anti_alias: true,
-        ..Default::default()
-    };
-
-    let stroke = Stroke {
-        width: 4.0,
-        ..Default::default()
-    };
-
-    let mut prev_index = 0;
-
-    for &end_index in &glyph_data.contour_end_indices {
-        let mut pb = PathBuilder::new();
-        let start_index = prev_index;
-
-        let first_point = &glyph_data.points[start_index];
-        pb.move_to(first_point.x as f32 + offset_x, (height as i64 - first_point.y) as f32 - offset_y);
-
-        for i in start_index + 1..=end_index {
-            let point = &glyph_data.points[i];
-            pb.line_to(point.x as f32 + offset_x, (height as i64 - point.y) as f32 - offset_y);
+impl Renderer {
+    pub fn new(offset_x: f32, offset_y: f32, width: u32, height: u32) -> Self {
+        Renderer {
+            offset: Point {
+                x: offset_x,
+                y: offset_y,
+            },
+            pixmap: Pixmap::new(width, height).unwrap(),
+            stroke: Stroke {
+                width: 4.0,
+                ..Default::default()
+            },
         }
-
-        // let mut current_point_idx = start_index + 2;
-
-        // while current_point_idx <= end_index {
-        //     let control_point = &glyph_data.points[current_point_idx - 1];
-        //     let point = &glyph_data.points[current_point_idx];
-        //     pb.quad_to(
-        //         control_point.x as f32,
-        //         control_point.y as f32,
-        //         point.x as f32,
-        //         point.y as f32,
-        //     );
-        //     current_point_idx += 2;
-        // }
-
-        pb.close();
-
-        if let Some(contour) = pb.finish() {
-            contours.push(contour);
-        }
-
-        prev_index = end_index + 1;
     }
 
-    for contour in contours.iter() {
-        pixmap.stroke_path(contour, &paint, &stroke, Transform::identity(), None);
+    pub fn draw_glyph(&mut self, glyph_data: &GlyphData) -> Result<(), Box<dyn std::error::Error>> {
+        let mut contours = Vec::with_capacity(glyph_data.contour_end_indices.len());
+
+        let paint = Paint {
+            anti_alias: true,
+            ..Default::default()
+        };
+
+        let mut prev_index = 0;
+
+        for &end_index in &glyph_data.contour_end_indices {
+            let mut pb = PathBuilder::new();
+            let start_index = prev_index;
+
+            let first_point = self.convert_glyph_point(glyph_data.points[start_index]);
+            pb.move_to(first_point.x, first_point.y);
+
+            let mut current_point_idx = start_index + 2;
+
+            while current_point_idx <= end_index {
+                let control_point =
+                    self.convert_glyph_point(glyph_data.points[current_point_idx - 1]);
+                let point = self.convert_glyph_point(glyph_data.points[current_point_idx]);
+                pb.quad_to(control_point.x, control_point.y, point.x, point.y);
+                current_point_idx += 2;
+            }
+
+            pb.close();
+
+            if let Some(contour) = pb.finish() {
+                contours.push(contour);
+            }
+
+            prev_index = end_index + 1;
+        }
+
+        for contour in contours.iter() {
+            self.pixmap
+                .stroke_path(contour, &paint, &self.stroke, Transform::identity(), None);
+        }
+
+        // Save the pixmap to a PNG file
+        let mut file = File::create("glyph.png")?;
+        file.write_all(self.pixmap.encode_png()?.as_ref())?;
+
+        Ok(())
     }
 
-    // Save the pixmap to a PNG file
-    let mut file = File::create("glyph.png")?;
-    file.write_all(pixmap.encode_png()?.as_ref())?;
-
-    Ok(())
+    pub fn convert_glyph_point(&self, point: GlyphPoint) -> Point {
+        Point {
+            x: point.x as f32 + self.offset.x,
+            y: (self.pixmap.height() as i64 - point.y) as f32 - self.offset.y,
+        }
+    }
 }
